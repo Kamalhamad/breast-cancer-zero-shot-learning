ab5964080ab31817d48123f19382ec9753295710
Kamal Hamad <kamal.hamad@ue-germany.de>
2026-02-16 12:24:11 +0100

    Update train.py

--- src/src/train.py
+++ src/src/train.py
@@ -1,1 +1,16 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder
 
+
+def train_classifier(features, labels):
+    """
+    Train logistic regression classifier on seen classes.
+    """
+    encoder = LabelEncoder()
+    encoded_labels = encoder.fit_transform(labels)
+
+    clf = LogisticRegression(max_iter=1000)
+    clf.fit(features, encoded_labels)
+
+    return clf, encoder
