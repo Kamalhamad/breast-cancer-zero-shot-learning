3b4e0cf4f5f6725d80cc389dd970808bc2556e12
Kamal Hamad <kamal.hamad@ue-germany.de>
2026-02-16 12:24:43 +0100

    Update zsl_inference.py

--- src/src/src/zsl_inference.py
+++ src/src/src/zsl_inference.py
@@ -1,1 +1,21 @@
+import numpy as np
+from embeddings import cosine_similarity
 
+
+def predict_unseen(feature_vector, classifier, encoder, class_embeddings):
+    """
+    Predict unseen class using embedding similarity.
+    """
+    predicted_label_index = classifier.predict([feature_vector])[0]
+    predicted_label = encoder.inverse_transform([predicted_label_index])[0]
+
+    best_match = None
+    best_score = -1
+
+    for class_name, embedding in class_embeddings.items():
+        score = cosine_similarity(feature_vector, embedding)
+        if score > best_score:
+            best_score = score
+            best_match = class_name
+
+    return best_match
