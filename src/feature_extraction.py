a35b5c9e2a359dacb4033e6d3807a2274a680c25
Kamal Hamad <kamal.hamad@ue-germany.de>
2026-02-16 12:21:06 +0100

    Update feature_extraction.py

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,1 +1,21 @@
+import numpy as np
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.preprocessing import image
 
+
+class FeatureExtractor:
+    def __init__(self):
+        self.model = ResNet50(weights="imagenet", include_top=False, pooling="avg")
+
+    def extract(self, img_path):
+        """
+        Extract feature vector from image using ResNet50.
+        """
+        img = image.load_img(img_path, target_size=(224, 224))
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array = preprocess_input(img_array)
+
+        features = self.model.predict(img_array)
+        return features.flatten()
